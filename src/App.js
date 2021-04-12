4a9e0d534d3f40f0ef74fed8450a24fc5f0c2251
Nikolay Lutsenko <nik.lu@ex.ua>
2021-04-12 23:07:12 +0300

    React learning. Part 3 React components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import restaurant from "./restaurant.jpg"
+
+function Header(props){
+  return(
+    <header>
+      <h1>
+        {props.name}'s Kitchen
+      </h1>
+    </header>
+  )
+}
+
+function Main(props){
+  return(
+    <section>
+      <p>
+        We serve the most {props.adjective} food around.
+      </p>
+      <img src={restaurant} height={200} alt="napkin and fork at a restaurant tabel"/>
+      <ul style={{textAlign:"left"}}>
+        {props.dishes.map((dish)=>(
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+function Footer(props){
+  return(
+    <footer>
+      <p>
+        Copyright {props.year}
+      </p>
+    </footer>
+  )
+}
+
+const dishes = [
+  "Pizza",
+  "Borsh",
+  "Pureshka",
+  "Kotletki"
+];
+
+const dishObj = dishes.map((dish,i)=>({id:i,title:dish}));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Nikolay"/>
+      <Main adjective="amaizing" dishes={dishObj}/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
