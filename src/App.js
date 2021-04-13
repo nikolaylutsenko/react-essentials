2ed74e600193a4aca562d5605fe526f5a963d97b
Nikolay Lutsenko <nik.lu@ex.ua>
2021-04-14 00:36:06 +0300

    Add more btns

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App(props) {
   const [emotion, setEmotion] = useState("happy");
+
+  useEffect(()=>{
+    console.log(`It's ${emotion} around here!`)
+  })
 
   return(
   <>
