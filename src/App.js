cac8e9f9be2a253514ec6f4fa16d6071b4d8c70e
Nikolay Lutsenko <nik.lu@ex.ua>
2021-04-14 00:56:34 +0300

    Use useReducer hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, {useState} from "react";
+import React, {useReducer} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [checked, setChecked] = useState(false);
-
-  function toggle(){
-    setChecked((checked)=>!checked);
-  }
-
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+    );
+  
   return(
   <>
    <input type="checkbox" value={checked} onChange={toggle}></input>
